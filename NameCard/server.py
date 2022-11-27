7155d5e8aa367347ccac4abd8e5f1b7c3f6edd4b
Shahid Khan <shahid98@live.com>
2022-11-27 18:12:13 -0500

    (Web Development) With flask using templates

---
+++ NameCard/server.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello(name=None):
+    return render_template('index.html', name=name)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+# document.body.contentEditable=true
